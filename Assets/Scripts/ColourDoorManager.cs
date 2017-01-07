2eb96f497880278762a41ba768e3996075c841a6
Leo <leojh.li@mail.utoronto.ca>
2017-01-07 16:47:37 -0500

    Created first level

---
+++ Assets/Scripts/ColourDoorManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourDoorManager : MonoBehaviour {
+
+	public void TryOpen (string colour) {
+		// TODO: test player colour against door's colour
+		/*
+		if (true)
+		{	
+			this.gameObject.SetActive (false);
+		}
+		*/
+	}
+}
