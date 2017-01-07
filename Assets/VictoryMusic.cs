74346351d4307102750d552783b9f3056250ddc7
Allen Lee <Allenyuklun.lee@mail.utoronto.ca>
2017-01-07 16:40:14 -0500

    Background music, bump music, and victory music

---
+++ Assets/VictoryMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryMusic : MonoBehaviour {
+
+	public AudioSource audio;
+	public AudioClip victory;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		audio.clip = victory;
+	}
+	
+	void OnTriggerEnter2D(Collider2D coll){
+		print ("JUMP");
+		audio.Play ();
+	}
+}
