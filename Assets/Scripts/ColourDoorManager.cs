5d137053801abfbe6c4523e300796d15824508de
Leo <leojh.li@mail.utoronto.ca>
2017-01-07 23:48:57 -0500

    removed scripts from player, fixed prefabs, added darkness to stage 2

--- Assets/Scripts/ColourDoorManager.cs
+++ Assets/Scripts/ColourDoorManager.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class ColourDoorManager : MonoBehaviour {
 
 	public static void TryOpen (Color playerColor, Color doorColor, Collision2D coll ) {
-
-		// TODO: test player colour against door's colour
 		if (playerColor.a == doorColor.a && playerColor.b == doorColor.b && playerColor.g == doorColor.g) {
 			print ("Pass the colour door");
 			coll.collider.enabled = false;
